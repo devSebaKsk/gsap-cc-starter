c11ec946af15b325d59c21eb82e1eb1d96a41cd9
Sebastian Cascante <139644093+devSebaKsk@users.noreply.github.com>
2025-06-09 01:45:51 +0000

    gsap timeline

--- src/pages/GsapTimeline.jsx
+++ src/pages/GsapTimeline.jsx
@@ -1,6 +1,37 @@
+import gsap from "gsap";
+import { useGSAP} from "@gsap/react";
+
 const GsapTimeline = () => {
   // TODO: Implement the gsap timeline
-
+  const timeline = gsap.timeline({
+    repeat: -1,
+    repeatDelay: 1,
+    yoyo: true,
+  });
+  useGSAP(() => {
+    timeline.to("#yellow-box", {
+      x: 250,
+      rotation: 360,
+      duration: 2,
+      ease: "back.inOut",
+      borderRadius: "100%",
+    })
+    timeline.to("#yellow-box", {
+      y: 250,
+      rotation: 0,
+      duration: 2,
+      ease: "back.inOut",
+      borderRadius: "0%",
+    })
+    timeline.to("#yellow-box", {
+      x:500,
+      rotation: 720,
+      scale: 0.5,
+      duration: 2,
+      ease: "back.inOut",
+      borderRadius: "2px",
+    })
+  },[])
   return (
     <main>
       <h1>GsapTimeline</h1>
@@ -35,7 +66,13 @@ const GsapTimeline = () => {
       </p>
 
       <div className="mt-20 space-y-10">
-        <button onClick={() => {}}>Play/Pause</button>
+        <button onClick={() => {
+          if (timeline.isActive()) {
+            timeline.pause();
+          } else {
+            timeline.play();
+          }
+        }}>Play/Pause</button>
 
         <div id="yellow-box" className="w-20 h-20 bg-yellow-500 rounded-lg" />
       </div>
